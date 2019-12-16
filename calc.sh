3898b4996560c6ca69dd9feef06c2254964ba6b4
saptarshi <saptarshi2001@gmail.com>
2019-12-16 23:43:11 +0530

    fixed error in calc.py

--- calc.sh
+++ calc.sh
@@ -3,14 +3,14 @@ if [ $# -ne 1 ];then
 fi
 
 file=$( cat $1 | tr " " "_" )
+#echo $file
 ans=0
 for i in $file;do
 	l=${#i}
-	if [[ ! ${i:0:l-2} =~ ^[0-9]+$ ]] || [[ ! ${i:l-1:l} =~ ^[+-*/]+$ ]] || [[ ! ${i:l-2:l-1} =~ ^[_]+$ ]];then
+	if [[ ! "${i:0:l-2}" =~ ^[0-9]+$ ]] || [[ ! "${i:l-1:1}" =~ ^["/*\+\-"]+$ ]] || [[ ! "${i:l-2:1}" =~ ^[_]+$ ]];then
 		exit -1
 	fi
-	exp="$ans ${i:l-1:l} ${i:0:l-2}"
-	echo "$exp"
+	exp="$ans ${i:$l-1:1} ${i:0:$l-2}"
 	ans=$(echo "$exp" | bc)
 done
 echo $ans
